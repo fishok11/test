913395b709dadb53e47b5551ee4972f2819ff530
fishok11 <svllsd811@gmail.com>
2024-02-13 15:03:08 +0300

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
 
 
 function App() {
   return (
     <div >
+      <Routes>
+        <Route index element={<HomePage/>}/>
+      </Routes>
 
     </div>
   );
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const HomePage: FC = () => {
+    return (
+        <div>
+            i am HOME PAGE!!!!!
+        </div>
+    )
+}
+
+export default HomePage;
