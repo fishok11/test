7d00df6afd904723564d84044d8b83f062a5db06
fishok11 <svllsd811@gmail.com>
2024-02-21 10:06:32 +0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// @type {import('tailwindcss').Config}
+module.exports = {
+  content: ['./src/**/*.{js, jsx, ts, tsx}'],
+  theme: {
+    extend: {
+      // boxShadow: {
+      //   xl: '0px 5px 10px 2px rgba(34, 60, 80, 0.2)',
+      //   serch: '0px 5px 8px 2px rgba(34, 60, 80, 0.12)',
+      // },
+    },
+  },
+  plugins: [],
+};
