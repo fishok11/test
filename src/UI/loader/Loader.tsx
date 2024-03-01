21236bd7560b385d2c3e59e823cb0d93d10cc106
fishok11 <svllsd811@gmail.com>
2024-03-01 13:32:32 +0300

    loader done

--- src/UI/loader/Loader.tsx
+++ src/UI/loader/Loader.tsx
@@ -2,7 +2,11 @@ import React, { FC } from 'react';
 import styles from './Loader.module.scss';
 
 const Loader: FC = () => {
-  return <div className={styles.loader}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader}></div>
+    </div>
+  );
 };
 
 export default Loader;
